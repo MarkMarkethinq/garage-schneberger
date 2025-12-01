6ea78cc1a61fb8331e0d2849e28d75ba10d77732
MarkMarkethinq <mark@markethinq.nl>
2025-12-01 12:11:23 +0100

    Add container wrapper and spacing to shortcode block output

--- resources/blocks/shortcode/shortcode.php
+++ resources/blocks/shortcode/shortcode.php
@@ -7,5 +7,9 @@ $shortcode = get_field('shortcode');
 ?>
 
 <?php if ($shortcode) : ?>
-    <?php echo do_shortcode($shortcode); ?>
+<section class="py-16">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <?php echo do_shortcode($shortcode); ?>
+    </div>
+</section>
 <?php endif; ?>
