568634d0f8aee4fd19a1f4021e0914ec8e34c0e3
MarkMarkethinq <mark@markethinq.nl>
2025-12-01 12:14:46 +0100

    Remove container wrapper and spacing from shortcode block output

--- resources/blocks/shortcode/shortcode.php
+++ resources/blocks/shortcode/shortcode.php
@@ -7,9 +7,5 @@ $shortcode = get_field('shortcode');
 ?>
 
 <?php if ($shortcode) : ?>
-<section class="py-16">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <?php echo do_shortcode($shortcode); ?>
-    </div>
-</section>
+    <?php echo do_shortcode($shortcode); ?>
 <?php endif; ?>
